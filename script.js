25772f5aa5d7cf79f6a080db2eba61f8b3baf7a2
Iva93k <mandarina123mk@gmail.com>
2017-01-13 23:15:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var firstMenu = document.querySelector('.menu.first'),
+/*var firstMenu = document.querySelector('.menu.first'),
     secondMenu = document.querySelector('.menu.second'),
     thirdMenu = document.querySelector('.menu.third'),
 	fourthMenu = document.querySelector('.menu.fourth'),
@@ -59,4 +59,30 @@ sixthMenu.addEventListener('mouseover', function () {
 sixthMenu.addEventListener('mouseout', function () {
     sixthDropdown.style.display = 'none';
 });
+*/
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}

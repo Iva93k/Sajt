e3ec21304943be541e22650deff0a8047c9cbd1a
Iva93k <mandarina123mk@gmail.com>
2017-01-15 17:46:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,88 +1,17 @@
-/*var firstMenu = document.querySelector('.menu.first'),
-    secondMenu = document.querySelector('.menu.second'),
-    thirdMenu = document.querySelector('.menu.third'),
-	fourthMenu = document.querySelector('.menu.fourth'),
-	fifthMenu = document.querySelector('.menu.fifth'),
-	sixtMenu = document.querySelector('.menu.sixth');
+var slideIndex = 0;
+showSlides();
 
-var firstDropdown = document.querySelector('.dropdown.first'),
-    secondDropdown = document.querySelector('.dropdown.second'),
-    thirdDropdown = document.querySelector('.dropdown.third'),
-	fourthDropdown = document.querySelector('.dropdown.fourth'),
-	fifthDropdown = document.querySelector('.dropdown.fourth'),
-	sixthDropdown = document.querySelector('.dropdown.sixth');
-
-	firstMenu.addEventListener('mouseover', function () {
-    firstDropdown.style.display = 'inline';
-});
-
-firstMenu.addEventListener('mouseout', function () {
-    firstDropdown.style.display = 'none';
-});
-
-secondMenu.addEventListener('mouseover', function () {
-    secondDropdown.style.display = 'inline';
-});
-
-secondMenu.addEventListener('mouseout', function () {
-    secondDropdown.style.display = 'none';
-});
-
-thirdMenu.addEventListener('mouseover', function () {
-    thirdDropdown.style.display = 'inline';
-});
-
-thirdMenu.addEventListener('mouseout', function () {
-    thirdDropdown.style.display = 'none';
-});
-
-fourthMenu.addEventListener('mouseover', function () {
-    fourthDropdown.style.display = 'inline';
-});
-
-fourthMenu.addEventListener('mouseout', function () {
-    fourthDropdown.style.display = 'none';
-});
-
-fifthMenu.addEventListener('mouseover', function () {
-    fifthDropdown.style.display = 'inline';
-});
-
-fifthMenu.addEventListener('mouseout', function () {
-    fifthDropdown.style.display = 'none';
-});
-
-sixthMenu.addEventListener('mouseover', function () {
-    sixthDropdown.style.display = 'inline';
-});
-
-sixthMenu.addEventListener('mouseout', function () {
-    sixthDropdown.style.display = 'none';
-});
-*/
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+
